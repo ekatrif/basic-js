ed7c65b93270edd67de0fa2f4d3de3a50c115731
ekatrif <katiapervak@gmail.com>
2023-04-11 13:18:38 +0200

    feat: +1 task solved

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const obj1 = {};
+  const obj2 = {};
+  const arr1 = s1.split("");
+  const arr2 = s2.split("");
+  let commonChars = 0;
+
+  arr1.forEach(item => {
+    obj1[item] = s1.split(item).length - 1;
+  })
+
+  arr2.forEach(item => {
+    obj2[item] = s2.split(item).length - 1;
+  })
+
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  for (let i=0;i<keys1.length;i++) {
+    for (let j=0;j<keys2.length;j++) {
+      if (keys1[i] === keys2[j]) {
+        commonChars += Math.min(obj1[keys1[i]],obj2[keys2[j]])
+      }
+    }
+  }
+
+  return commonChars;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    constructor(isDirect = true) {
+      this.isDirect = isDirect;
+    }
+  
+    encrypt(message, key) {
+      if (!message || !key) {
+        throw new Error('Incorrect arguments!');
+      }
+  
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+      // числовое значение 'A' в ASCII  
+      const codeA = 65;
+      //число букв в алфавите
+      const lettersNumber = 26;
+  
+      let result = '';
+      let keyIndex = 0;
+  
+      for (let i = 0; i < message.length; i++) {
+        const currentChar = message.charCodeAt(i);
+  
+        if (currentChar >= codeA && currentChar < codeA + lettersNumber) {
+          const messageIndex = currentChar - codeA;
+          const keyChar = key.charCodeAt(keyIndex % key.length);
+          const keyIndexInAlphabet = keyChar - codeA;
+          let codedIndex = (messageIndex + keyIndexInAlphabet) % lettersNumber;
+          let codedChar = String.fromCharCode(codedIndex + codeA);
+  
+          result += codedChar;
+          keyIndex++;
+        } else {
+          result += message[i];
+        }
+      }
+  
+      return this.isDirect ? result : result.split('').reverse().join('');
+    }
+  
+    decrypt(encryptedMessage, key) {
+      if (!encryptedMessage || !key) {
+        throw new Error('Incorrect arguments!');
+      }
+  
+      encryptedMessage = encryptedMessage.toUpperCase();
+      key = key.toUpperCase();
+  
+      const codeA = 65;
+      const lettersNumber = 26;
+  
+      let result = '';
+      let keyIndex = 0;
+  
+      for (let i = 0; i < encryptedMessage.length; i++) {
+        const currentChar = encryptedMessage.charCodeAt(i);
+  
+        if (currentChar >= codeA && currentChar < codeA + lettersNumber) {
+          const encryptedIndex = currentChar - codeA;
+          const keyChar = key.charCodeAt(keyIndex % key.length);
+          const keyIndexInAlphabet = keyChar - codeA;
+          let decodedIndex =
+            (encryptedIndex + lettersNumber - keyIndexInAlphabet) %
+            lettersNumber;
+          let decodedChar = String.fromCharCode(decodedIndex + codeA);
+  
+          result += decodedChar;
+          keyIndex++;
+        } else {
+          result += encryptedMessage[i];
+        }
+      }
+  
+      return this.isDirect ? result : result.split('').reverse().join('');
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+
 
 module.exports = {
   VigenereCipheringMachine
